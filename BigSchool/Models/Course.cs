3fa65364b3b405b39c14f387aa62c6dd0b6926ec
Administrator <Administrator@PC224>
2020-06-05 11:32:10 +0700

    Add project files.

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+       
+        public int Id { get; set; }
+
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Caterogy { get; set; }
+        [Required]
+        public byte CategoryID { get; set; }
+        public byte CategoryId { get; internal set; }
+
+        
+    }
+
+
+}
